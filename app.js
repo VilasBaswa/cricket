2bc577f34b0811b983b9b96c2aebf4096349aa07
Vilas Baswa <baswavilas2000@gmail.com>
2023-01-01 12:06:30 +0000

    cricket2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { open } = require("sllite");
+const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
 const databasePath = path.join(__dirname, "cricketMatchDetails.db");
 
-const express = express();
+const app = express();
+
 app.use(express.json());
 
 let database = null;
@@ -14,34 +15,46 @@ const initializeDbAndServer = async () => {
   try {
     database = await open({
       filename: databasePath,
-      driver: sqlite3.database,
+      driver: sqlite3.Database,
     });
+
     app.listen(3000, () =>
-      console.log("server Running at http://localhost:3000/")
+      console.log("Server Running at http://localhost:3000/")
     );
   } catch (error) {
-    console.log(`DB Error:${error.message}`);
+    console.log(`DB Error: ${error.message}`);
     process.exit(1);
   }
 };
+
 initializeDbAndServer();
 
-const convertPlayerDbObjectToResponseObject = (dbObject) =>{
-    return {
-        playerId:dbObject.playerId,
-        playerName:dbObject.playerName,
-    };
+const convertPlayerDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+  };
 };
 
-app.get("/players/",(request,response)=>{
-    const getPlayersQuery = `
-        SELECT 
-        * 
-        FROM 
-        player_details;
-    `;
-    const statesArray = await database.all(getPlayersQuery);
-    response.send(
-        statesArray.map((eachElement)=>convertPlayerDbObjectToResponseObject(eachElement))
-    );
+const convertMatchDetailsDbObjectToResponseObject = (dbObject) => {
+  return {
+    matchId: dbObject.match_id,
+    match: dbObject.match,
+    year: dbObject.year,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const getPlayerQuery = `
+    SELECT
+      *
+    FROM
+      player_details;`;
+  const playersArray = await database.all(getPlayerQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertPlayerDbObjectToResponseObject(eachPlayer)
+    )
+  );
 });
+module.export = app;
